9837fd83fc25c3cc4b3bc89b69eaddd8f4fd14f9
PHIL KWEK <philkwek03@gmail.com>
2021-01-27 13:19:24 +0800

    27/1/2020: Added link to Login

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $('#create_account').click(function(){
     window.location = 'create_account/create_account.html'
 })
+
+$('#login_account').click(function(){
+    window.location = 'login_page/login.html'
+})
