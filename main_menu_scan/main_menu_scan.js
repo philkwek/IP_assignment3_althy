7ac498613f4c24edea8087bcf44f5fd0807d8b1b
PHIL KWEK <philkwek03@gmail.com>
2021-01-28 16:07:11 +0800

    28/1/2020: Removed error check due to bugs

--- main_menu_scan/main_menu_scan.js
+++ main_menu_scan/main_menu_scan.js
@@ -4,11 +4,6 @@ function onScanSuccess(qrCodeMessage) {
 	// handle on success condition with the decoded message
 }
 
-function onScanError(errorMessage) {
-    alert('Scanning failed!'); // handle on error condition, with error message
-    html5QrcodeScanner.clear();
-}
-
 var html5QrcodeScanner = new Html5QrcodeScanner(
 	"qr-reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess, onScanError);
+html5QrcodeScanner.render(onScanSuccess);
